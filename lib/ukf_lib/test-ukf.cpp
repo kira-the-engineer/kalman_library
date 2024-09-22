371913f1eb6f4955cde512021dac1655f6c86d4f
Kira Kopcho <kirak5683@gmail.com>
2024-09-22 15:23:10 -0700

    Created a pure cpp test file (not for running on a mcu) for testing individual functions of the UKF lib

---
+++ lib/ukf_lib/test-ukf.cpp
@@ -0,0 +1,11 @@
+/*
+ * File strictly for debugging/testing the UKF lib functions for development purposes. This allows for work to be done without the microcontroller
+ * being plugged in. This should not be used when wanting to benchmark the actual hardware performance of the filters.
+ */
+
+#include "ukf.hpp"
+#include <iostream>
+
+int main(){
+    
+}
