fa9f9b68233656ee14e1800a3b3445f0cd5c743a
Kira Kopcho <kirak5683@gmail.com>
2024-12-06 20:57:10 -0800

    Rename vectors and matricies for better readability/clarity on what each matrix is

--- lib/ukf_lib/ukf.cpp
+++ lib/ukf_lib/ukf.cpp
@@ -1,12 +1,11 @@
 #include "ukf.hpp"
 
-UKF::UKF(int x, int z, float time, float a, float b, MatrixXf (*process)(MatrixXf, float), MatrixXf (*meas)(MatrixXf), 
-    VectorXf (*nl_add)(VectorXf, VectorXf) = NULL, VectorXf (*nl_sub)(VectorXf, VectorXf) = NULL, float k = 0.0, int u = 1){
+UKF::UKF(int x, int z, float time, float a, float b, MatrixXf (*process)(MatrixXf, float), MatrixXf (*meas)(MatrixXf), float k = 0.0, int u = 1){
     //set dims first
-    this->x_dim = x;
-    this->z_dim = z;
-    this->u_dim = u;
-    this->num_sigmas = 2*this->x_dim + 1;
+    this->s_dim = x;
+    this->m_dim = z;
+    this->c_dim = u;
+    this->num_sigmas = 2*this->s_dim + 1;
 
     /*set floats*/
     this->dt = time;
@@ -15,50 +14,36 @@ UKF::UKF(int x, int z, float time, float a, float b, MatrixXf (*process)(MatrixX
     this->kappa = k;
 
     //Vector initialization
-    this->x.setZero(this->x_dim);
-    this->z.setZero(this->z_dim);
-    this->u.setZero(this->u_dim);
-    this->y.setZero(this->z_dim);
+    this->state_est.setZero(this->s_dim); //state est vector
+    this->data.setZero(this->m_dim); //measurements vector
+    this->control.setZero(this->c_dim); //control vector
+    this->residual.setZero(this->m_dim); //residual
 
     //row vector init
-    this->Wc.setZero(1, this->num_sigmas);
-    this->Wm.setZero(1, this->num_sigmas);
+    this->W_mean.setZero(1, this->num_sigmas);
+    this->W_cov.setZero(1, this->num_sigmas);
+
 
     //Weights and sigmas initialization
     this->set_weights(); //initializes the covariance and mean weight row vectors
 
     //initialize zero matricies for storing the transformed sigma points
-    this->sigmas_f.setZero(this->num_sigmas, this->x_dim);
-    this->sigmas_h.setZero(this->num_sigmas, this->z_dim);
+    this->sigmas_s.setZero(this->num_sigmas, this->s_dim);
+    this->sigmas_m.setZero(this->num_sigmas, this->m_dim);
 
     //set function pointers 
     this->f_x = process;
     this->h_x = meas;
-    this->nl_add = nl_add;
-    this->nl_sub = nl_sub;
 
     //Set matricies
-    this->P.setIdentity(this->x_dim, this->x_dim);
-    this->Q.setIdentity(this->x_dim, this->x_dim);
-    this->R.setIdentity(this->z_dim, this->z_dim);
-    this->K.setZero(this->x_dim, this->z_dim);
-    this->B.setZero(this->x_dim, this->u_dim);
-    this->S.setZero(this->z_dim, this->z_dim);
-    this->SI.setZero(this->z_dim, this->z_dim);   
-
-    //set flags
-    if(this->nl_add != NULL){
-        this->add = true;
-    }
-    else{
-        this->add = false;
-    }
-    if(this->nl_sub != NULL){
-        this->sub = true;
-    }
-    else{
-        this->sub = false;
-    }
+    this->state_cov.setIdentity(this->s_dim, this->s_dim);
+    this->proc_err.setIdentity(this->s_dim, this->s_dim);
+    this->meas_cov.setZero(this->m_dim, this->m_dim);
+    this->meas_covI.setZero(this->m_dim, this->m_dim);
+    this->meas_err.setIdentity(this->m_dim, this->m_dim);
+    this->gain.setZero(this->s_dim, this->m_dim);
+    this->control_mat.setZero(this->s_dim, this->c_dim);
+    this->cross_cov.setZero(this->s_dim, this->m_dim);
 }
 
 UKF::UKF(){
@@ -70,38 +55,30 @@ UKF::~UKF(){
 }
 
 void UKF::set_weights(){
-    this->lambda = powf(this->alpha, 2) * (this->x_dim + this->kappa) - this->x_dim;
-    this->Wc.fill(0.5 / (this->x_dim + this->lambda)); //Wc[i] = 1/(2(n + lambda)) where i = 1..2n
-    this->Wm.fill(0.5 / (this->x_dim + this->lambda)); //Wm[i] = 1/(2(n + lambda)) where i = 1..2n
-    this->Wc(0) = (this->lambda / (this->x_dim + this->lambda)) + (1.0 - powf(this->alpha, 2) + this->beta); //Wc[0] = lambda/(n + lambda) + 1 - alpha^2 + beta
-    this->Wm(0) = this->lambda / (this->x_dim + this->lambda); //Wm[0] = lambda/(n + lambda)
+    this->lambda = powf(this->alpha, 2) * (this->s_dim + this->kappa) - this->s_dim;
+    this->W_cov.fill(0.5 / (this->s_dim + this->lambda)); //Wc[i] = 1/(2(n + lambda)) where i = 1..2n
+    this->W_mean.fill(0.5 / (this->s_dim + this->lambda)); //Wm[i] = 1/(2(n + lambda)) where i = 1..2n
+    this->W_cov(0) = (this->lambda / (this->s_dim + this->lambda)) + (1.0 - powf(this->alpha, 2) + this->beta); //Wc[0] = lambda/(n + lambda) + 1 - alpha^2 + beta
+    this->W_mean(0) = this->lambda / (this->s_dim + this->lambda); //Wm[0] = lambda/(n + lambda)
 }
 
 MatrixXf UKF::generate_sigmas(VectorXf x, MatrixXf P){
     MatrixXf sigmas, chol;
     VectorXf row_i; //vectors to store chol(i)
-    sigmas.setZero(this->num_sigmas, this->x_dim); //initialize empty matrix of (2n+1, n) where n is the dim of the state
+    sigmas.setZero(this->num_sigmas, this->s_dim); //initialize empty matrix of (2n+1, n) where n is the dim of the state
 
-    chol = P * (this->lambda + this->x_dim);
+    chol = P * (this->lambda + this->s_dim);
     chol = chol.llt().matrixU(); //take sqrt (cholesky decomp) of (n+lamba)P and return an upper triangular view
 
     //irst row of the sigma point matrix is the means
     sigmas.row(0) = x.col(0); //assigns row 0 to elements in the column vector x
     
-
-    if(this->sub == true) { //if nonlinear values are in the matrices/vectors
-        for(int i = 0; i < this->x_dim; i++){
-            sigmas.row(i + 1) = this->nl_sub(-x, -row_i);
-            sigmas.row(i + this->x_dim + 1) = this->nl_sub(x, row_i);
-        }
-    }
-    else{
-        for(int i = 0; i < this->x_dim; i++){
+    for(int i = 0; i < this->s_dim; i++){
             row_i = chol.row(i);
             sigmas.row(i + 1) = x.col(0) + row_i;
-            sigmas.row(i + this->x_dim + 1) = x.col(0) - row_i;
-        }
+            sigmas.row(i + this->s_dim + 1) = x.col(0) - row_i;
     }
+    
 
     return sigmas;
 }
--- lib/ukf_lib/ukf.hpp
+++ lib/ukf_lib/ukf.hpp
@@ -17,10 +17,10 @@ class UKF {
         /*
          * Constructor for a basic unscented kalman filter object.
          * Parameters:
-         * dim_x (int): dimension of the state vector- pertains to the number of state variables used in the filter (i.e how many rows in the vector)
-         * dim_z (int): dimension of the measurement vector- changes based on if you have multiple sensors and what each sensor outputs. For instance
+         * dim_s (int): dimension of the state vector- pertains to the number of state variables used in the filter (i.e how many rows in the vector)
+         * dim_m (int): dimension of the measurement vector- changes based on if you have multiple sensors and what each sensor outputs. For instance
          * if you have a single position sensor with outputs (x,y) z would be a column vector with 2 rows. 
-         * dim_u (int): dimension of the control vector, where number of rows is the number of control inputs. defaults to 1 by 1 vector
+         * dim_c (int): dimension of the control vector, where number of rows is the number of control inputs. defaults to 1 by 1 vector
          * dt (float): time step used in the state transition process- in seconds
          * process (function): Function that moves the sigma points forward in time according to the process model. Should return a matrix of transformed 
          * sigma points. Takes in the sigma points matrix and the time step used to forward each point
@@ -28,11 +28,9 @@ class UKF {
          * alpha (float): determines spread of sigma points around the mean when generating points (small positive value btwn 0 and 1)
          * beta (float): incorporates prior knowledge of the distribution of the mean
          * kappa (float): Secondary scaling parameter- most often set to zero according to Van der Merwe's paper but sometimes set to 3-n for gaussians
-         * nl_add (function) - optional. For use with state/measurement variables that are nonlinear like angles
-         * nl_subtract (function) - optional. For use with state/measurement variables that are nonlinear like angles
          */ 
-        UKF(int dim_x, int dim_z, float time, float alpha, float beta, MatrixXf (*process)(MatrixXf, float), 
-            MatrixXf (*meas)(MatrixXf), VectorXf (*nl_add)(VectorXf, VectorXf), VectorXf (*nl_sub)(VectorXf, VectorXf), float kappa, int dim_u);
+        UKF(int dim_s, int dim_m, float time, float alpha, float beta, MatrixXf (*process)(MatrixXf, float), 
+            MatrixXf (*meas)(MatrixXf), float kappa, int dim_c);
 
         ~UKF(); //deconstructor
 
@@ -49,12 +47,12 @@ class UKF {
         /*
          * Computes the sigma points for the UKF using Van Der Merwe's method
          * Parameters:
-         * x (vector): n-length vector that holds the mean/state of the filter
-         * P (matrix): Covariance of filter
+         * mean (vector): n-length vector that holds the mean/state of the filter
+         * cov (matrix): Covariance of filter
          * Returns:
          * sigmas: Matrix of size (2*dim_x+1, n). Each col contains sigma points for 1 dimension of the problem space
          */
-        MatrixXf generate_sigmas(VectorXf x, MatrixXf P);
+        MatrixXf generate_sigmas(VectorXf mean, MatrixXf cov);
 
         /*
          * Performs the unscented transform according to Van der Merwe's Paper- called in both update and predict fxns
@@ -69,38 +67,40 @@ class UKF {
         void unscented_transform(MatrixXf sigmas, RowVectorXf wm, RowVectorXf wc, MatrixXf r_q, VectorXf* xp_zp, MatrixXf* Pp_Pz);
     
     private:
-        int x_dim, z_dim, u_dim; //dimensions that get updated with parameter values
+        int s_dim, m_dim, c_dim; //dimensions that get updated with parameter values
         int num_sigmas; //number of sigma points to generate
         float dt; //time step
-        bool add; //flag to use non-linear add function
-        bool sub; //flag to use non-linear subtract function
 
         /*sigma parameters*/
         float beta, kappa, alpha, lambda;
 
         /*Vectors and Matricies*/
-        VectorXf x, z, u, y; //state, measurement, control, and residual vectors
-        //P: current state covariance matrix <x rows, x cols>. Any call to the update or predict updates this variable
-        //R: measurement noise matrix <z rows, z cols>
-        //Q: process noise matrix
-        //K: Kalman gain <x rows, z cols>
-        //S: System uncertainty (P projected to measurement space)
-        //SI: System uncertainty Inverse
-        //B: control transition matrix <x rows, u cols>, default is a zero matrix of size dim_x, dim_u
-        //u: control vector, default is a zero vector of length dim_u
-        //Wm: Matrix of mean weights for sigma points as calculated using Van der Merwe's method for generating sigmas
-        //Wc: Matrix of covariance weights for sigma points as calculated using Van der Merwe's method for generating sigmas
-        //sigmas_f: Matrix that stores sigma points forwarded in time by f_x, of dimensions (2*dim_x + 1), dim_x
-        //sigmas_h: Matrix that stores sigma points projected to the measurement domain by h_x, of dimensions (2*dim_x + 1), dim_z
-        //y: residual
-        MatrixXf P, R, Q, K, S, SI, B, sigmas_f, sigmas_h;
-        RowVectorXf Wc, Wm;
-    
+        
+        //Vectors
+        //state_est: vector of dimension s_dim. Stores the estimate of the state (mean). Updated whenever the predict-update sequence is called. Denoted as "x" in literature
+        //control: vector of dimension c_dim. Not always used, but stores whatever control signal is passed to the system. Denoted as "u" in literature
+        //data: vector of dimension m_dim. Stores data from sensors/signals. Denoted as "z" in literature
+        //residual: vector that's updated whenever update is called. The error between the data points and their calculated mean
+        VectorXf state_est, control, data, residual;
+        RowVectorXf W_mean, W_cov; //Row vectors that hold the weights for the mean and covariance as calculated in Van Der Merwe's paper
+
+        //Matricies
+        //state_cov: Covariance matrix for the state estimate <dim_s rows, dim_s cols>. Any call to the update or predict updates this variable (P in literature)
+        //meas_cov: Also known as the system uncertainty. Covariance of the sigma points transformed by h_x. <dim_m rows, dim_m cols> (S in literature)
+        //meas_covI: inverse of measurement covariance, used in calculating the kalman gain (SI in literature)
+        //meas_err: Error/noise in data/measurement. <dim_m rows, dim_m cols>
+        //proc_err: Error/noise in the process model. <dim_s rows, dim_s cols>
+        //cross_cov: Matrix to store the cross covariance between the state and the data/measurements <dim_s rows, dim_m cols>
+        //gain: Kalman gain. In the case of the UKF we can think of gain as a ratio of our belief in the state estimate vs our belief in the measurement
+        //sigmas_s: Matrix that stores the sigma points transformed by f_x (the process sigmas).  <num_sigmas rows, dim_s cols>
+        //sigmas_m: Matrix that stores the sigma points transformed by h_x (the measurment sigmas). <num_sigmas rows, dim_m cols>
+        //control_mat: Optional matrix used for storing the transition function for systems with control inputs. <dim_s rows, dim_c cols>
+        MatrixXf state_cov, meas_cov, meas_covI, meas_err, proc_err, cross_cov, gain, sigmas_s, sigmas_m, control_mat;
+
+
         //function pointers
         MatrixXf (*f_x)(MatrixXf, float) = NULL;
         MatrixXf (*h_x)(MatrixXf) = NULL;
-        VectorXf (*nl_sub)(VectorXf, VectorXf) = NULL;
-        VectorXf (*nl_add)(VectorXf, VectorXf) = NULL;
 
         void set_weights(); // Calculates weights according to Van Der Merwe's paper- should only be called in constructor
 
